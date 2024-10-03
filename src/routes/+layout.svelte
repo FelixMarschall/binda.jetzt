ce0a106d271f2b6cb34ff5010cfcba2748e6e1ed
Felix Marschall <felix.mars@outlook.com>
2024-10-03 22:59:56 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script>
+    import "../app.css";
+
+	let isMenuOpen = false;
+    const currentYear = new Date().getFullYear();
+
+</script>
+<html lang="de" data-theme="cupcake"></html>
+<div class="min-h-screen flex flex-col items-center">
+    <div class="navbar bg-base-100 mx-auto max-w-6xl p-4 flex justify-between items-center">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl" href="/" >BinDa</a>
+        </div>
+        <div class="flex-none gap-2">
+          <input type="checkbox" value="dracula" class="toggle theme-controller" />
+          <a class="btn btn-ghost text-l" href="/" data-sveltekit-preload-data>Account</a>
+          <!-- <a class="btn btn-ghost text-l" href="/settings" >Einstellungen</a> -->
+        </div>
+      </div>
+    <!-- Main Content -->
+    <main class="flex-grow container mx-auto max-w-6xl p-4">
+        <slot />
+    </main>
+
+    <!-- Footer -->
+    <footer class="footer bg-base-200 text-base-content p-10">
+      <aside> 
+        <!-- <p>
+          ACME Industries Ltd.
+          <br />
+          Providing reliable tech since 1992
+        </p> -->
+      </aside>
+      
+      <div>
+        <h6 class="footer-title text-xl" style="opacity: 1;">Kontakt</h6>
+        <p>
+          Wir freuen uns über deine Nachricht!
+          </p>
+          <a href="mailto:kontakt@softghost.dev" class="underline">kontakt@binda.jetzt</a>
+      </div>
+      <!-- <nav>
+        <h6 class="footer-title">Company</h6>
+        <a class="link link-hover">About us</a>
+        <a class="link link-hover">Contact</a>
+        <a class="link link-hover">Jobs</a>
+        <a class="link link-hover">Press kit</a>
+      </nav> -->
+      <nav>
+        <h6 class="footer-title">Legal</h6>
+        <a class="link link-hover" href="/impressum">Impressum</a>
+        <a class="link link-hover" href="/datenschutz">Datenschutz</a>
+        <!-- <a class="link link-hover">Cookie policy</a> -->
+      </nav>
+    </footer>
+</div>
