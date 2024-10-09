c2c0bce3ac7f9d19390143e97ed214de91f0c73c
Felix Marschall <felix.mars@outlook.com>
2024-10-09 20:36:07 +0200

    only return db resources

--- src/routes/services/+server.ts
+++ src/routes/services/+server.ts
@@ -16,12 +16,11 @@ export const GET: RequestHandler = async ({ request, cookies }) => {
 			]
 		}).fetchAll();
 	
-		return new Response(JSON.stringify(result), { headers: { 'Content-Type': 'application/json' } });
+		return new Response(JSON.stringify(result.resources), { headers: { 'Content-Type': 'application/json' } });
 	} catch (error) {
 		console.log('Error validating token: ', error);
 		return new Response(`Error validating token:\t${error}`, { status: 401 });
 	}
-	
 }
 
 export const fallback: RequestHandler = async ({ request }) => {
